7dbb121b9de37eeaa7a4ac108f5a68634394a42d
tomla <tomla@192.168.0.123>
2018-10-14 18:30:12 +0200

    Set up Spring project

---
+++ src/main/java/hu/elte/absencetracker/AbsenceTrackerApplication.java
@@ -0,0 +1,12 @@
+package hu.elte.absencetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsenceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsenceTrackerApplication.class, args);
+	}
+}
