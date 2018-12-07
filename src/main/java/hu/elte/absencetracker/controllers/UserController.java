15c3c76a8febf9e05b91088ab09310dc563c5f52
tomla <tomla@192.168.0.123>
2018-12-07 11:14:10 +0100

    User controller now supports taking and removing lessons

--- src/main/java/hu/elte/absencetracker/controllers/UserController.java
+++ src/main/java/hu/elte/absencetracker/controllers/UserController.java
@@ -3,13 +3,16 @@ package hu.elte.absencetracker.controllers;
 import hu.elte.absencetracker.entities.Absence;
 import hu.elte.absencetracker.entities.Lesson;
 import hu.elte.absencetracker.entities.User;
+import hu.elte.absencetracker.repositories.LessonRepository;
 import hu.elte.absencetracker.repositories.UserRepository;
 import hu.elte.absencetracker.security.AuthenticatedUser;
+import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,6 +30,9 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepository;
+    
+    @Autowired
+    private LessonRepository lessonRepository;
 
     @Autowired
     private BCryptPasswordEncoder passwordEncoder;
@@ -75,6 +81,56 @@ public class UserController {
         user.setEnabled(true);
         user.setRole(User.Role.STUDENT);
         return ResponseEntity.ok(userRepository.save(user));
+    }
+        
+    @PostMapping("/{id}/activeLessons")
+    public ResponseEntity<User> addActiveLesson(@PathVariable Integer id, @RequestBody Lesson lesson) {
+        Optional<User> oUser = userRepository.findById(id);
+        if(oUser.isPresent()) {
+            User authUser = authenticatedUser.getUser();
+            User user = oUser.get();
+            if(authUser.getId().equals(user.getId()) || authUser.getRole() == User.Role.ADMIN){
+                Optional<Lesson> oLesson = lessonRepository.findById(lesson.getId());
+                if(oLesson.isPresent()) {
+                    lesson = oLesson.get();
+                    if(!lesson.getStudents().contains(user)) {
+                        lesson.getStudents().add(user);
+                        lessonRepository.save(lesson);
+                    }
+                    return ResponseEntity.ok(user);
+                } else {
+                    return ResponseEntity.notFound().build();
+                }
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    
+    @DeleteMapping("/{id}/activeLessons/{lessonId}")
+    public ResponseEntity<User> addActiveLesson(@PathVariable Integer id, @PathVariable Integer lessonId) {
+        Optional<User> oUser = userRepository.findById(id);
+        if(oUser.isPresent()) {
+            User authUser = authenticatedUser.getUser();
+            User user = oUser.get();
+            if(authUser.getId().equals(user.getId()) || authUser.getRole() == User.Role.ADMIN) {
+                Optional<Lesson> oLesson = lessonRepository.findById(lessonId);
+                if(oLesson.isPresent()) {
+                    Lesson lesson = oLesson.get();
+                    if(!lesson.getStudents().remove(user)) return ResponseEntity.notFound().build();
+                    lessonRepository.save(lesson);
+                    return ResponseEntity.ok(user);
+                } else {
+                    return ResponseEntity.notFound().build();
+                }
+            } else {
+                return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+            }
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 
     @GetMapping("/{id}/activeLessons")
